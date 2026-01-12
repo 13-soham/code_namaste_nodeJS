35c7c7032c4ec1e8a36914bf422dd9c6ada265fe
13-soham <sohammondal821@gmail.com>
2026-01-12 23:56:16 +0530

    till_12thJan_2026

--- Basic/database.js
+++ Basic/database.js
@@ -1,1 +1,26 @@
-const URI = "mongodb+srv://NamasteNodeJs:hosterdb9432@namastenodejs.exkc5xt.mongodb.net/";
+const mongoose = require('mongoose');
+const express = require("express");
+
+const URI = "mongodb+srv://NamasteNodeJs:TyC04SJFdhZfxH6Z@namastenodejs.exkc5xt.mongodb.net/";
+const app = express();
+const port = 8000;
+
+
+const connectDb = async ()=>{
+    try{
+        await mongoose.connect(URI);
+        console.log("db connection success");
+    }
+    catch(err){
+        console.log(err.message);
+    }
+}
+
+app.get("/", (req, res)=>{
+    res.send("hello");
+});
+
+app.listen(port, ()=>{
+    connectDb();
+    console.log(`server listen at port ${port}`);
+});
